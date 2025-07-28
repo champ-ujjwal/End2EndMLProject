4a9979e9003515ee31a4b49c88415af523b2d3c3
UJJWAL KUMAR <2204ujjwalkumar@gmail.com>
2025-07-28 13:45:04 +0530

    ml flow setup

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 This is End to End Data Science project where i am implementing all the things from scratch. Here i am predicting maths_score using various ML algorithms.
+## ML FLOW
+export MLFLOW_TRACKING_URI=https://dagshub.com/champ-ujjwal/End2EndDS.mlflow
+export MLFLOW_TRACKING_USERNAME=champ-ujjwal
+export MLFLOW_TRACKING_PASSWORD=95b8e63f3dc0ef97498c0af0ede0b9ebfe45e8ca
+python app.py
+
+
+
--- src/mlproject/components/model_tranier.py
+++ src/mlproject/components/model_tranier.py
@@ -1,7 +1,11 @@
 import os
 import sys
 from dataclasses import dataclass
-
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
+import numpy as np
+from sklearn.metrics import mean_squared_error,mean_absolute_error
 from catboost import CatBoostRegressor
 from sklearn.ensemble import (
     AdaBoostRegressor,
@@ -19,31 +23,36 @@ from src.mlproject.logger import logging
 from src.mlproject.utils import save_object,evaluate_models
 
 
-
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path: str = os.path.join("artifacts", "model.pkl")  # path to save the trained model
+    trained_model_file_path=os.path.join("artifacts","model.pkl")
 
 class ModelTrainer:
     def __init__(self):
-        self.model_trainer_config = ModelTrainerConfig()
+        self.model_trainer_config=ModelTrainerConfig()
 
-    def initiate_model_trainer(self, train_array, test_array):
+    def eval_metrics(self,actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2
+
+    def initiate_model_trainer(self,train_array,test_array):
         try:
-            logging.info("Splitting training and testing input data")
-            X_train, y_train, X_test, y_test = (
-                train_array[:, :-1],
-                train_array[:, -1],
-                test_array[:, :-1],
-                test_array[:, -1],
+            logging.info("Split training and test input data")
+            X_train,y_train,X_test,y_test=(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
             )
-            models ={
+            models = {
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
                 "XGBRegressor": XGBRegressor(),
-                "CatBoosting Regressor": CatBoostRegressor(verbose=0),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
             params={
@@ -80,18 +89,74 @@ class ModelTrainer:
                     'learning_rate':[.1,.01,0.5,.001],
                     # 'loss':['linear','square','exponential'],
                     'n_estimators': [8,16,32,64,128,256]
-                }     
+                }
+                
             }
             model_report:dict=evaluate_models(X_train,y_train,X_test,y_test,models,params)
 
-            ## To get the best model and its parameters from dict
+            ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
 
-            ## To get the best model and its parameters from dict
+             ## To get best model name from dict
+
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
-            best_model=models[best_model_name]
+            best_model = models[best_model_name]
+
+            print("This is the best model:")
+            print(best_model_name)
+
+            model_names = list(params.keys())
+
+            actual_model=""
+
+            for model in model_names:
+                if best_model_name == model:
+                    actual_model = actual_model + model
+
+            best_params = params[actual_model]
+
+            # mlflow.set_registry_uri("https://dagshub.com/champ-ujjwal/End2EndMLProject.mlflow")
+            # tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+            
+
+            mlflow.set_tracking_uri("https://dagshub.com/champ-ujjwal/End2EndMLProject.mlflow")
+            mlflow.set_experiment("MyExperiment")  # Set the experiment name
+
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+
+
+            # mlflow
+
+            with mlflow.start_run():
+
+                predicted_qualities = best_model.predict(X_test)
+
+                (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
+
+                mlflow.log_params(best_params)
+
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)
+
+
+                # Model registry does not work with file store
+                if tracking_url_type_store != "file":
+
+                    # Register the model
+                    # There are other ways to use the Model Registry, which depends on the use case,
+                    # please refer to the doc for more information:
+                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                    mlflow.sklearn.log_model(best_model, "model", registered_model_name=actual_model)
+                else:
+                    mlflow.sklearn.log_model(best_model, "model")
+
+
+
 
             if best_model_score<0.6:
                 raise CustomException("No best model found")
@@ -107,7 +172,7 @@ class ModelTrainer:
             r2_square = r2_score(y_test, predicted)
             return r2_square
 
-        
+
+
         except Exception as e:
-            raise CustomException(e, sys)
-        
+            raise CustomException(e,sys)
